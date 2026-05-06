1a581321242e8dcc6cbfca35f14622309d1a3f19
Yuvaraj-Kumaran <yuvaraj-kumaran@users.noreply.github.com>
2026-05-06 08:36:51 +0530

    user login is complete

--- apps/user-ui/src/app/(routes)/login/page.tsx
+++ apps/user-ui/src/app/(routes)/login/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
+import { useMutation } from '@tanstack/react-query';
 import GoogleButton from 'apps/user-ui/src/shared/components/google-button';
+import axios, { AxiosError } from 'axios';
 import { Eye, EyeOff } from 'lucide-react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
@@ -25,7 +27,29 @@ const Login = () => {
         formState: {errors},
     } = useForm<FormData>();
 
-    const onSubmit = (data:FormData) => {}
+    const loginMutation = useMutation({
+        mutationFn: async(data:FormData) => {
+            const response = await axios.post(`${process.env.NEXT_PUBLIC_SERVER_URI}/api/login-user`,
+                data,
+                {withCredentials: true},
+            );
+            return response.data
+        },
+        onSuccess: (data) => {
+            setServerError(null);
+            router.push("/");
+        },
+        onError: (error: AxiosError) => {
+            const errorMessage = (error.response?.data as {message?: string})?.message || "Invalid Credentials";
+
+            setServerError(errorMessage);
+        },
+    });
+
+    const onSubmit = (data:FormData) => {
+        console.log(data);
+        loginMutation.mutate(data);
+    }
 
   return (
     <div className = "w-full py-10 min-h-[85vh] bg-[#f1f1f1]">
@@ -114,8 +138,11 @@ const Login = () => {
                         </Link>
                 </div>
 
-                    <button type="submit" className='w-full text-lg cursor-pointer bg-black text-white py-2 rounded-lg'>
-                        Login
+                    <button 
+                    type="submit" 
+                    disabled= {loginMutation.isPending }
+                    className='w-full text-lg cursor-pointer bg-black text-white py-2 rounded-lg'>
+                        {loginMutation.isPending ? "Loggin in ..." : "Login"}
                     </button>
 
                 {serverError && (
